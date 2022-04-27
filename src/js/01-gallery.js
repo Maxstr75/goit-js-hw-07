c5d16723b57017427065dfac5a80a51182ba7a98
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-04-27 22:46:20 +0300

    add modal

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,15 +2,14 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 
-const instance = basicLightbox.create(`
-    <div class="modal">
-        <p>
-            Your first lightbox with just a few lines of code.
-            Yes, it's really that simple.
-        </p>
-    </div>
-`)
+const instance = basicLightbox.create(`<img src="${modalImgToShow}" width="800" height="600">`,
+{        closable: true,
+ onShow: (instance) => { window.addEventListener('keydown', onModalPressEsc) },
+ onClose: (instance) => {window.removeEventListener('keydown', onModalPressEsc)},
+ });
 
-instance.show()
+instance.show()  
+    
+
 
 console.log(galleryItems);
