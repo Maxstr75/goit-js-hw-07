14cab4fd9adb9901feab0af540f863b7ac71935b
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-04-28 20:05:07 +0300

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>
 
 const instance = basicLightbox.create(`<img src="${modalImgToShow}" width="800" height="600">`,
 {        closable: true,
