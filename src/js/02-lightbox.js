67cec1f9af641e7bc04235a56be25cc298c26fca
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-04-29 14:05:14 +0300

    add js & css

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,10 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+
+<a class="gallery__item" href="large-image.jpg">
+  <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+</a>
+
 console.log(galleryItems);
